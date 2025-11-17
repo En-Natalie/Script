1b03cbca938b88f280405fbc74cf39dfec4bd203
Thanh-Nhon <90920521+Thanh-Nhon@users.noreply.github.com>
2025-11-17 12:32:17 -0600

    hiding key hopefully

--- Script/functionality/env.js
+++ Script/functionality/env.js
@@ -1,5 +1,5 @@
 // env.js
-export const HUGGING_FACE_KEY = "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";
+export const HUGGING_FACE_KEY = "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";
 // Set this to the Hugging Face model id you want to use, e.g.:
 // "facebook/blip-image-captioning-base" or "microsoft/resnet-50"
-export const HUGGING_FACE_MODEL = "Salesforce/blip-image-captioning-base";
+export const HUGGING_FACE_MODEL = "google/vit-base-patch16-224";
--- Script/functionality/env.ts
+++
@@ -1,7 +0,0 @@
-// env.ts
-export const HUGGING_FACE_KEY = "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";
-// Set this to the Hugging Face model id you want to use, e.g.:
-// "Salesforce/blip-image-captioning-base" or "google/vit-base-patch16-224"
-// You can also set a full inference router URL if you use a custom router, but
-// a simple model id is sufficient for the hosted inference API.
-export const HUGGING_FACE_MODEL = "google/vit-base-patch16-224";
